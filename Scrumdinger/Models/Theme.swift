e913883dc5eb6d9f036be2f05471474be35d5baa
Jay Ryu <jayhuanryu@gmail.com>
2022-01-18 21:57:31 +0900

    Creating a Card View

---
+++ Scrumdinger/Models/Theme.swift
@@ -0,0 +1,44 @@
+//
+//  Theme.swift
+//  Scrumdinger
+//
+//  Created by Jay Ryu on 2022/01/18.
+//
+
+import Foundation
+import SwiftUI
+
+enum Theme : String {
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavender
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seafoam
+    case sky
+    case tan
+    case teal
+    case yellow
+    
+    var accentColor: Color {
+        switch self {
+            case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow:
+                return .black
+            case .indigo, .magenta, .navy, .oxblood, .purple:
+                return .white
+        }
+    }
+    
+    var mainColor : Color {
+        Color(rawValue)
+    }
+    
+    
+
+}
